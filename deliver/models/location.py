e0f59959281e6ce6651187b1a2bb785748d6ef0c
Endy Iskandar Imam <imam.endy@gmail.com>
2020-10-25 02:27:51 -0400

    Integrate preliminary Locations model.
    - Single property requirement integration into Locations model.

--- deliver/models/location.py
+++ deliver/models/location.py
@@ -6,7 +6,9 @@ Hierachy of Classes:
   - Address: str
 """
 
-from pydantic import BaseModel, confloat
+from typing import Optional
+
+from pydantic import BaseModel, confloat, constr, root_validator
 
 
 class Coordinate(BaseModel):
@@ -16,12 +18,28 @@ class Coordinate(BaseModel):
         latitude (float): The latitude of the coordinate
         longitude (float): The longitude of the coordinate
     """
-    latitude:  confloat(ge=- 90, le= 90)
-    longitude: confloat(ge=-180, le=180)
+    latitude:  confloat(ge=- 90, le= 90) = 0
+    longitude: confloat(ge=-180, le=180) = 0
 
 
+# TODO: Maybe add Address Validation
+
+NEITHER_MSG = "Either 'coordinate' and/or 'address' must have a value"
 # TODO: Create Location model
 class Location(BaseModel):
-    """TODO: Add Docstring
+    """Class for location data, coordinate and/or address
+
+    Attributes (at least one are required):
+        coordinate (Coordinate): The exact coordinate of the location
+        address (str): A written address
     """
-    pass
+    coordinate: Optional[Coordinate]
+    address: Optional[constr(min_length=1)]
+
+    @root_validator
+    def at_least_one_required(cls, values):
+        """Ensure that there is either a coordinate or address
+        """
+        if values.get('coordinate') is None and values.get('address') is None:
+            raise ValueError(NEITHER_MSG)
+        return values
--- tests/models/test_location.py
+++ tests/models/test_location.py
@@ -4,7 +4,8 @@
 import pytest
 from pydantic import ValidationError
 
-from deliver.models.location import Coordinate
+from deliver.models.location import Coordinate, Location, NEITHER_MSG
+
 
 
 @pytest.mark.parametrize('latitude' , [- 90, 0,  90])
@@ -27,3 +28,36 @@ def test_invalid_coordinate_assignment(latitude, longitude):
     if latitude != 0 and longitude != 0:
         with pytest.raises(ValidationError):
             Coordinate(latitude=latitude, longitude=longitude)
+
+
+
+def test_valid_location_assignment():
+    """Testing valid Location assignment
+
+    Testing that at least one of those fields are accepted
+    """
+    coordinate = Coordinate()
+    address = '20 W 34th St, New York, NY 10001'
+    Location(address=address)
+    Location(coordinate=coordinate)
+    Location(coordinate=coordinate, address=address)
+
+
+def test_invalid_location_assignment_none():
+    """Testing invalid Location assignment w/ no arguments
+
+    Testing if it neither have coordinate or address output a
+    ValidationError
+    """
+    with pytest.raises(ValidationError) as excval:
+        Location()
+    assert NEITHER_MSG in str(excval)
+
+
+def test_invalid_location_assignment_address():
+    """Testing invalid Location assignment w/ empty address
+
+    Testing if an empty address string returns a ValidationError
+    """
+    with pytest.raises(ValidationError):
+        Location(address='')
