ee7e43482d5013c5ea3d713dfced4bcd0caff029
Endy Iskandar Imam <imam.endy@gmail.com>
2020-11-02 17:21:41 -0500

    Refactor requirements and integrate Mangum.

--- deliver/main.py
+++ deliver/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from mangum  import Mangum
+
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
+handler = Mangum(app)
